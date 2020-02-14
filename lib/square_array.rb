1a03809bee8ffb3a0f9291767d37f4ed648a4a91
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-14 21:34:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,14 @@
-def square_array(array)
-  # your code here
+def square_array(numbers = [1,2,3])
+  result = Array.new(numbers)
+  i = 0
+  #sum = Math.sqrt(array.each)
+  while i < numbers.size do
+    result[i] = numbers[i] ** 2
+    i += 1
+    puts result
+  end
 end
+
+#numbers = [1,2,3]
+
+#square_array(numbers)
